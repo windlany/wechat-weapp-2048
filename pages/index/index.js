dd72b26f52f9626b04d02c29f54a611ac59f6808
lan <17713476019@163.com>
2018-01-23 15:08:16 +0800

    添加首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,56 +4,28 @@ const app = getApp()
 
 Page({
   data: {
-    motto: '欢迎来到2048小游戏~',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    current: 0
+  },  
+  onReady: function() {
+    this.load();
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  click: function() {
-    wx.navigateTo({
-      url: '../2048/2048',
-    })
-  },
-  onLoad: function () {  // 加载完毕获取用户信息
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+  load: function() {
+    var n = 1;
+    var timer = setInterval(()=>{
+      if(n == 6) {
+        clearInterval(timer);
+        wx.redirectTo({
+          url: '../2048/2048'
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+      this.setData({
+        current: this.data.current+1
+      });
+      if(this.data.current > 3) 
+        this.setData({
+          current: 0
+        });
+        n++;
+    }, 400);
   }
 })
